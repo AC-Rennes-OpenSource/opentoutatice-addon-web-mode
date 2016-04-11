f357faecac85482ec7bb9d358de20bd916488696
no_author <contact@osivia.com>
2016-04-11 09:27:48 +0000

    #1153 : contrôle d'unicité lors de la saisie manuelle du segmenr Url

--- opentoutatice-addon-web-mode-ecm/src/main/java/org/osivia/opentoutatice/web/bean/WebModeValidatorBean.java
+++ opentoutatice-addon-web-mode-ecm/src/main/java/org/osivia/opentoutatice/web/bean/WebModeValidatorBean.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.osivia.opentoutatice.web.bean;
 
@@ -14,9 +14,14 @@ import javax.faces.validator.ValidatorException;
 
 import org.apache.commons.lang.StringUtils;
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 import org.nuxeo.ecm.platform.ui.web.util.ComponentUtils;
 
 import fr.toutatice.ecm.platform.core.constants.ExtendedSeamPrecedence;
@@ -30,11 +35,18 @@ import fr.toutatice.ecm.platform.core.constants.ExtendedSeamPrecedence;
 @Scope(ScopeType.SESSION)
 @Install(precedence = ExtendedSeamPrecedence.TOUTATICE)
 public class WebModeValidatorBean implements Serializable {
-    
+
     private static final long serialVersionUID = 5623376301895535136L;
-    
-    final Pattern patternSegment = Pattern.compile("([a-zA-Z_0-9\\-]+)");
-    
+
+    private static final Pattern patternSegment = Pattern.compile("([a-zA-Z_0-9\\-]+)");
+
+    @In(create = true, required = true)
+    protected transient CoreSession documentManager;
+
+    @In(create = true, required = true)
+    protected NavigationContext navigationContext;
+
+
     public void validateSegment(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         if(StringUtils.isNotBlank((String) value)){
             Matcher m = patternSegment.matcher((String) value);
@@ -43,7 +55,33 @@ public class WebModeValidatorBean implements Serializable {
                 FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null);
                 throw new ValidatorException(message);
             }
+            if (!validateUnicity((String) value)) {
+                String msg = ComponentUtils.translate(context, "webmode.validator.existing.segment");
+                FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null);
+                throw new ValidatorException(message);
+            }
         }
+    }
+
+
+    private boolean validateUnicity(String value) {
+
+        final DocumentModel doc = navigationContext.getCurrentDocument();
+
+        final DocumentModelList siblings = documentManager.getChildren(doc.getParentRef());
+
+        if (siblings != null) {
+            for (DocumentModel documentModel : siblings) {
+                if (!StringUtils.startsWith(documentModel.getPathAsString(), doc.getPathAsString())) {
+                    final String segmentUrl = (String) documentModel.getPropertyValue("ottcweb:segment");
+                    if (StringUtils.equals(value, segmentUrl)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 
 }
